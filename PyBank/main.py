97bd58a28c0beec8813bd24c0048790733e80c8e
Andrew Miller <amiller9108@gmail.com>
2021-04-02 17:59:54 -0400

    PyBank data paths defined, total months, and net p&l calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,3 +14,31 @@ date_increase = 0
 greatest_decrease = 0
 date_decrease = 0
 
+# Path to the budget data from the resources folder
+budgetdata_csv = os.path.join('..', 'Resources', 'budget_data.csv')
+
+#
+with open(budgetdata_csv, newline=' ') as budgetdata
+
+    # Budget Reader splits data at the comma and reads the header line first 
+    budgetreader = csv.reader(budgetdata, delimiter=',')
+    header = next(budgetreader)
+    row = next(budgetreader)
+
+    # Calculate total months, net "P&L" and set variables
+    total_months += 1
+    net_total = int(row[1])
+    greatest_increase = int(row[1])
+    date_increase = int(row[1])
+
+
+    for row in budgetreader:
+
+        # Calculate the total number of months
+        total_months += 1
+
+        # Calculate net P&L
+        net_total = int(row[1])
+        
+
+
