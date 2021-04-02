3d650d50e08d6613e83e5f0e7c9fad0e14d78fb1
Andrew Miller <amiller9108@gmail.com>
2021-04-02 16:59:13 -0400

    Defined Variables and Import Modules

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+# HW 3 | Python Assignment - Py Me Up, Charlie (PyBank Script)
+
+# Import Modules
+Import os
+Import csv
+
+# Defining Variables
+total_months = 0
+net_total = 0
+month_count = []
+monthly_change = []
+greatest_increase = 0
+date_increase = 0
+greatest_decrease = 0
+date_decrease = 0
+
